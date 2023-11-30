b86df747ef6e523065e6da113b0e51657f1d15fb
ls1w <michelle_j.b@hotmail.com>
2023-11-30 11:11:38 -0300

    Triângulo

---
+++ javascript/main.js
@@ -0,0 +1,59 @@
+'use strict'
+
+const numero1 = document.getElementById('numero1');
+const numero2 = document.getElementById('numero2');
+const numero3 = document.getElementById('numero3');
+const resultado = document.getElementById('resultado');
+
+function Limpar() {
+    numero1.value = '';
+    numero2.value = '';
+    numero3.value = '';
+    imgTriangulo.setAttribute('src','');
+    resultado.innerHTML = '-';
+}
+
+function Verificar() {
+    if (!(testarNumeros())) {
+        alert('Digite valores inteiros maiores que 0');
+        return false;
+
+    } else {
+        definirTriangulo();
+    }
+}
+
+// FUNÇÃO DE TEST SE NÃO SÃO VAZIOS OU MENOR QUE ZERO
+function testarNumeros() {
+    if (numero1.value <= 0) {
+        numero1.focus();
+        return false;
+
+    } else if (numero2.value <= 0) {
+        numero2.focus();
+        return false;
+
+    } else if (numero3.value <= 0) {
+        numero3.focus();
+        return false;
+
+    } else {
+        return true;
+    }
+}
+
+// FUNÇÃO DEFINIR O NOME DO TRIANGULO
+function definirTriangulo() {
+    if (numero1.value == numero2.value && numero1.value == numero3.value) {
+        resultado.innerHTML = 'É UM TRIÂNGULO EQUILÁTERO!';
+        imgTriangulo.setAttribute('src','./img/Equilatero.png');
+
+    } else if (numero1.value == numero2.value || numero1.value == numero3.value || numero2.value == numero3.value) {
+        resultado.innerHTML = 'É UM TRIÂNGULO ISÓSCELES!';
+        imgTriangulo.setAttribute('src','./img/isosceles.png');
+
+    } else {
+        resultado.innerHTML ='É UM TRIÂNGULO ESCALENO!';
+        imgTriangulo.setAttribute('src','./img/escaleno.png');
+    }
+}  
